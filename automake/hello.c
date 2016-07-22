1c58d12d973f638931476009de5356f718d03736
Mohammad H. Mofrad <mohammad.h.mofrad@gmail.com>
2016-07-22 00:23:24 -0400

    Create hello.c

---
+++ automake/hello.c
@@ -0,0 +1,16 @@
+/*
+ * File: hello world test program
+ * Author: Mohammad H. Mofrad
+ * Email: hasanzadeh@cs.pitt.edu
+ * Date: July 2016; Last revision 07-22-2016
+ * Copyright (C) Mohammad H. Mofrad - All Rights Reserved
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(int argc, char *argv[])
+{
+     printf("Hello, World!\n");
+     return (0);
+}
