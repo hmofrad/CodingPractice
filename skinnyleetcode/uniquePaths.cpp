d7b23e7046da92403f424ec9dff86d94ef230578
hmofrad <m.hasanzadeh.mofrad@gmail.com>
2018-12-08 16:29:58 -0500

    Update uniquePaths.cpp

---
+++ skinnyleetcode/uniquePaths.cpp
@@ -0,0 +1,29 @@
+/*
+ *  Problem: Implement Pow(x, n)
+ *  Compile: g++ -o uniquePaths uniquePaths.cpp -std=c++11 && ./uniquePaths
+ *  Execute: ./uniquePaths
+ *  (c) Mohammad Mofrad, 2018
+ *  (e) m.hasanzadeh.mofrad@gmail.com
+ */
+
+#include <iostream>
+#include <stdint.h>
+#include <stdio.h>
+#include <vector>
+#include <stdlib.h>
+#include <utility>
+#include <algorithm>
+#include <string.h>
+
+int uniquePaths(int m, int n) 
+{
+    int num_pathes = 0;
+    return(num_pathes);
+}
+
+int main(int argc, char **argv)
+{
+    int m = 3, n = 2;
+    printf("uniquePaths(%d, %d)=%d\n", m, n, uniquePaths(m, n));
+    return(0);
+}
