9f502d28c7258b70be5894d6bffc1ed6386d2b4c
hmofrad <m.hasanzadeh.mofrad@gmail.com>
2019-02-28 08:19:36 -0500

    Update minimumTotal.cpp

---
+++ skinnyleetcode/minimumTotal.cpp
@@ -0,0 +1,34 @@
+/*
+ *  Compile & Execute: g++ -o minimumTotal minimumTotal.cpp -std=c++11 && ./minimumTotal
+ *  (c) Mohammad Hasanzadeh Mofrad, 2019
+ *  (e) m.hasanzadeh.mofrad@gmail.com
+ */
+
+#include <iostream>
+#include <stdint.h>
+#include <stdio.h>
+#include <vector>
+#include <stdlib.h>
+#include <utility>
+#include <algorithm>
+#include <string.h>
+
+int minimumTotal(std::vector<std::vector<int>>& triangle) {
+    int path = 0;
+    int m = triangle.size();
+    for(int i = 0; i < m; i++) {
+        std::vector<int>& t = triangle[i];
+        for(int j: t) {
+            printf("%d ", j);
+        }
+        printf("\n");
+    }
+    return(0);
+}
+
+
+int main(int argc, char** argv) {
+    std::vector<std::vector<int>> triangle = {{2}, {3,4}, {6,5,7}, {4,1,8,3}};
+    printf("minimumTotal=%d\n", minimumTotal(triangle));
+    return(0);
+}
