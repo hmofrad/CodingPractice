9d7316d5c28e180fa4d13a69e363a77c532d575f
hmofrad <m.hasanzadeh.mofrad@gmail.com>
2019-09-17 00:00:10 -0400

    Update queue.cpp

--- crackingthecodinginterview/queue.cpp
+++ crackingthecodinginterview/queue.cpp
@@ -8,35 +8,107 @@
 #include <stdio.h>
 
 struct Node {
-    Node* next = nullptr;
-    int data;
-    Node() {};
-    Node(int d) {data = d;}
+    public:
+        Node() {};
+        Node(int d) {data = d;}
+        
+        Node* next = nullptr;
+        int data = 0;
+        
+        //void appendToTail(Node** head, int d);
+        
+        
 };
 
-void Node::appendToTail(int d) {
-        
-        //Node* end = new Node(d);
-        printf("0000 %p\n",head );
-        //
-        if(head == nullptr) {
-            printf("xxxx\n");
-            Node* h = new Node(d);
-            *head = h;
-            printf("111\n");
+//void Node::
+void appendToTail(Node** head, int d) {        
+    //Node* end = new Node(d);
+    //printf("0000 %p %p %d\n",head, *head, (*head)->data);
+    //
+    
+    if((*head) == nullptr) {
+        //printf("xxxx\n");
+        (*head) = new Node(d);
+        //printf("111\n");
+    }
+    
+    
+    else {
+
+        //Node* h = (*head); 
+        //int i = 0;
+        while((*head)->next != nullptr) {
+            //i++;
+            //printf("%d\n", i);
+            (*head) = (*head)->next;
+        }
+        (*head)->next = new Node(d);  
+    }
+    
+}
+
+
+void deleteFromHead(Node** head) {  
+    if((*head) != nullptr) {
+        if((*head)->next == nullptr) {
+            delete (*head);
+            (*head) = nullptr;
         }
         else {
-            printf("xxxxddd\n");
-            Node* h = (*head); 
-            int i = 0;
-            while(h->next != nullptr) {
-                i++;
-                printf("%d\n", i);
-                h = h->next;
-            }
-            h->next = new Node(d);  
+            Node* node = (*head)->next;
+            delete (*head);
+            (*head) = node;
         }
     }
+}  
+
+
+void traverseQueue(Node* head) {
+    //Node* head = head;
+    
+    //if(head == nullptr) {
+      //  printf("%d\n", head->data);
+    //}
+    //else {
+    if(head != nullptr) {
+        while(head->next != nullptr) {
+            printf("%d ", head->data);
+            head = head->next; 
+        }
+        printf("\n");
+    }
+    //}
+}
+
+int main(int argc, char**argv) {
+    printf("Hello queue\n");
+    
+    Node* head = nullptr;
+    //traverseQueue(head);
+    appendToTail(&head, 0);
+    Node* head_ = head;
+    //head->appendToTail(&head_, 1);
+    
+    
+    for(int i = 1; i < 10; i++) {
+        head_ = head;
+        appendToTail(&head_, i);
+        head_ = head;
+        traverseQueue(head);
+    }
+    head_ = head;
+    traverseQueue(head);
+    head_ = head;
+    for(int i = 0; i < 10; i++) {
+        deleteFromHead(&head);
+    //head_ = head;
+        traverseQueue(head);
+    }
+    
+    
+    return(0);
+}
+
 /*
 class Queue {
     public:
@@ -93,17 +165,3 @@ class Queue {
 };
 */
 
-
-int main(int argc, char**argv) {
-    printf("Hello queue\n");
-    
-    Queue queue;
-    
-    for(int i = 0; i < 10; i++) {
-        queue.appendToTail(i);
-    }
-    //queue.traverseQueue();
-    
-    
-    return(0);
-}
