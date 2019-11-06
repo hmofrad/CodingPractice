48fa2c3546cd5179acc6e7ff1e7a3c11f84ed875
hmofrad <m.hasanzadeh.mofrad@gmail.com>
2019-11-06 09:21:46 -0500

    Update wordPattern.cpp

---
+++ skinnyleetcode/wordPattern.cpp
@@ -0,0 +1,92 @@
+    /*
+ *  Problem: https://leetcode.com/problems/word-pattern/
+ *  Compile: g++ -o wordPattern wordPattern.cpp -std=c++11 -fsanitize=address && ./wordPattern
+ *  (c) Mohammad Hasanzadeh Mofrad, 2019
+ *  (e) m.hasanzadeh.mofrad@gmail.com
+ */
+
+#include <iostream>
+#include <stdint.h>
+#include <stdio.h>
+#include <vector>
+#include <stdlib.h>
+#include <utility>
+#include <algorithm>
+#include <string.h>
+#include <unordered_set>
+#include <stack>
+#include <numeric>
+#include <map>
+#include <cmath>
+#include <unordered_map>
+
+void print(std::vector<int> nums) {
+    for(auto n: nums)
+        printf("%d ", n);
+    printf("\n");
+}
+
+//bool custom_insert(std::map<char, std::string>& hash, ) {
+    
+//}
+
+bool wordPattern(std::string pattern, std::string str) {
+    bool tf = true;
+    std::map<char, std::string> hash;
+    int m = pattern.size();
+    int n = str.size();
+    std::string sub_str;
+    int j = 0;
+    for(int i = 0; i < n; i++) {
+        if(str[i] != ' ') {
+            sub_str += str[i];
+        }
+        
+        if((str[i] == ' ') or (i == n-1)) {
+            printf("%c: %s\n", pattern[j], sub_str.c_str());
+            if(hash.find(pattern[j]) == hash.end()) {
+                for (auto it = hash.begin(); it != hash.end(); it++) {
+                    if (it->second == sub_str.c_str()) {
+                        std::cout << ">>>>"  << it->first << " " << it->second << "\n";
+                        tf = false;
+                        break;
+                    }
+                }
+                if(not tf) break;
+                hash.insert({pattern[j], sub_str});                
+            }
+            else {
+                if(hash[pattern[j]] != sub_str) {
+                    tf = false;
+                    break;
+                }
+            }
+            if(not tf) break;
+            j++;
+            sub_str.clear();
+        }
+    }
+    if(j != m) tf = false;
+
+    for(auto& h : hash)
+        std::cout << h.first << " " <<  h.second << std::endl;
+    
+    return(tf);
+}
+    
+int main(int argc, char **argv){
+    std::string pattern = "abba";
+    std::string str = "dog cat cat dog";
+    
+    printf("%s, %s\n", pattern.c_str(), str.c_str());
+    bool f = wordPattern(pattern, str);
+    printf("wordPattern() ? %d\n", f);
+    
+    return(0);
+}
+
+
+
+
+
+
