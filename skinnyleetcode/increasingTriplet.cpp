545452225edb40a2b0657f9222124f02a5f74ae1
hmofrad <m.hasanzadeh.mofrad@gmail.com>
2019-11-19 11:06:25 -0500

    Update increasingTriplet.cpp

---
+++ skinnyleetcode/increasingTriplet.cpp
@@ -0,0 +1,85 @@
+    /*
+ *  Problem: https://leetcode.com/problems/increasing-triplet-subsequence/
+ *  Compile: g++ -o increasingTriplet increasingTriplet.cpp -std=c++11 && ./increasingTriplet #-fsanitize=address 
+ *  (c) Mohammad Hasanzadeh Mofrad, 2019
+ *  (e) m.hasanzadeh.mofrad@gmail.com
+ */
+
+#include <iostream>
+#include <stdint.h>
+#include <stdio.h>
+#include <vector>
+#include <stdlib.h>
+#include <utility>
+#include <algorithm>
+#include <string.h>
+#include <unordered_set>
+#include <stack>
+#include <numeric>
+#include <map>
+#include <cmath>
+#include <unordered_map>
+/*
+bool increasingTriplet(std::vector<int>& nums) {
+    bool triplet = false;
+    for(int i = 0 ; i < nums.size(); i++) {
+        int m = i + 3;
+        if(m <= nums.size()) {
+            printf("%d %d %d\n", i, nums[i], m);
+            bool found = true;
+            for(int j = i; j < m-1; j++) {
+                if(nums[j] >= nums[j+1]) {
+                    found = false;
+                    break;
+                }                    
+            }
+            if(found) {
+                triplet = true;
+                break;
+            }
+        }
+    }
+    return(triplet);    
+}
+*/
+
+bool increasingTriplet(std::vector<int>& nums) {
+    if(nums.size() < 3) return(false);
+    int n = nums.size();
+    std::vector<bool> dp(n, false);
+    for(int i = 0; i < n; i++) {
+        for(int j = 0; j < i; j++) {
+            if(nums[i] > nums[j]) {
+                if(dp[j]) return(true);
+                else dp[i] = true;
+            }
+        }
+        
+        for(int i = 0; i < n; i++) {
+            printf("%d ", dp[i] == true);
+        }
+        printf("\n");
+    }
+    
+
+    
+    
+    return(false);
+}
+
+
+int main(int argc, char **argv){
+    std::vector<int> nums = {5,1,5,5,2,5,4};//{1,1,1,1,1,1};// {1,2,3,4,5}; //{5,4,3,2,1};//
+    bool found = increasingTriplet(nums);
+    for(auto&n : nums)
+        printf("%d ", n);
+    printf("? %d\n", found);
+    
+    return(0);
+}
+
+
+
+
+
+
