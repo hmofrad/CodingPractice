6c17978bbbf41c3e97b3c9ca3290cb8adee1e0e7
hmofrad <m.hasanzadeh.mofrad@gmail.com>
2019-12-15 23:06:46 -0500

    Update kthSmallest_matrix.cpp

--- skinnyleetcode/kthSmallest_matrix.cpp
+++ skinnyleetcode/kthSmallest_matrix.cpp
@@ -10,6 +10,7 @@
 #include <iostream>
 #include <vector>
 
+/*
 int kthSmallest(std::vector<std::vector<int>>& matrix, int k) {
     k--;
     uint32_t m = matrix.size();
@@ -19,9 +20,48 @@ int kthSmallest(std::vector<std::vector<int>>& matrix, int k) {
     printf("%d %d %d %d %d \n", m, n, i, j, k);
     return(matrix[i][j]);        
 }
+*/
+
+int kthSmallest(std::vector<std::vector<int>>& matrix, int k) {
+    std::vector<int> smallests(matrix.size());
+    std::vector<uint32_t> indices(matrix.size());
+    int smallest = 0;
+    
+    for(uint32_t i = 0; i < matrix.size(); i++) {
+        smallests[i] = matrix[i][0];
+    }
+    
+    
+    for(int32_t l = 0; l < k; l++) {
+        //for(uint32_t i = 0; i < matrix.size(); i++) {
+          //  if(indices[i] < matrix[0].size()) {
+            //    smallests[i] = matrix[i][indices[i]];
+            //}
+        //}
+        
+        uint32_t j = 0;
+        while(indices[j] >= matrix[0].size()) {
+            j++;
+        }
+        smallest = smallests[j];
+        int index = j;
+        for(uint32_t i = j; i < smallests.size(); i++) {
+            if(smallests[i] < smallest) {
+                smallest = smallests[i];
+                index = i;
+            }
+        }
+        indices[index]++;
+        if(indices[index] < matrix[0].size()) {
+            smallests[index] = matrix[index][indices[index]];
+        }
+    }
+    return(smallest);        
+}
+
 
 int main(int argc, char **argv){
-    std::vector<std::vector<int>> matrix = {{ 1,  5,  9}, {10, 11, 13}, {12, 13, 15}};
+    std::vector<std::vector<int>> matrix = {{ 1,  5,  9}, {10, 11, 13}, {12, 13, 15}}; //{{1,2},{1,3}};//
     int k = 8;
     int s = kthSmallest(matrix, k);
     printf("%d smallest = %d\n", k, s);
