c8f70706214bf1a44928943da6f7cad3eeeb83fb
hmofrad <m.hasanzadeh.mofrad@gmail.com>
2020-02-04 11:20:09 -0500

    Update runLengthEncoding.c

--- interviews/Microsoft/runLengthEncoding.c
+++ interviews/Microsoft/runLengthEncoding.c
@@ -2,7 +2,6 @@
  *  Problem: https://www.geeksforgeeks.org/run-length-encoding/
  *  Given an input string, write a function that returns the Run Length Encoded string for the input string.
  *  For example, if the input string is “wwwwaaadexxxxxx”, then the function should return “w4a3d1e1x6”.
- *  write a program to find the least common ancestor.
  *  Compile: gcc -o runLengthEncoding runLengthEncoding.c -g -Wall -fsanitize=address && ./runLengthEncoding    
  *  (c) Mohammad Hasanzadeh Mofrad, 2020
  *  (e) m.hasanzadeh.mofrad@gmail.com
