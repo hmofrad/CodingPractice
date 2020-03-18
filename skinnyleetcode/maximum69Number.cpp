2bcb8717e2113b864d3ce303ab86b4be32449ed0
hmofrad <m.hasanzadeh.mofrad@gmail.com>
2020-03-17 21:22:19 -0400

    Update maximum69Number.cpp

--- skinnyleetcode/maximum69Number.cpp
+++ skinnyleetcode/maximum69Number.cpp
@@ -1,42 +1,22 @@
 /*
- *  Problem: https://leetcode.com/problems/maximum-binary-tree/solution/
+ *  Problem: https://leetcode.com/problems/maximum-69-number/
  *  (c) Mohammad Hasanzadeh Mofrad, 2020
  *  (e) m.hasanzadeh.mofrad@gmail.com
  */
 
-/**
- * Definition for a binary tree node.
- * struct TreeNode {
- *     int val;
- *     TreeNode *left;
- *     TreeNode *right;
- *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
- * };
- */
 class Solution {
 public:
-    TreeNode* insert(vector<int>& nums, int left, int right) {
-        if(left > right) return(NULL);
-        
-        int max_val = nums[left];
-        int max_idx = left;
-        for(int i = left+1; i <= right; i++) {
-            if(nums[i] > max_val) {
-                max_val = nums[i];
-                max_idx = i;
+    int maximum69Number (int num) {
+        std::string s = std::to_string(num);
+        for(auto& c: s) {
+            if(c == '6') {
+                c = '9';
+                break;
             }
         }
         
-        if(left == right) return(new TreeNode(nums[max_idx]));
+        return(std::stoi(s));
         
-        TreeNode* root = new TreeNode(nums[max_idx]);
-        root->left = insert(nums, left, max_idx-1);
-        root->right = insert(nums, max_idx+1, right);
-        return(root);
-    }
-    
-    
-    TreeNode* constructMaximumBinaryTree(vector<int>& nums) {
-        return(insert(nums, 0, nums.size()-1));
     }
 };
+
