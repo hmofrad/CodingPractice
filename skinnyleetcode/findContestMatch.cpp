0d9feb65e23ad13d3d8e95ea63a0955d8b4ddfac
hmofrad <m.hasanzadeh.mofrad@gmail.com>
2020-03-19 16:30:50 -0400

    Update findContestMatch.cpp

---
+++ skinnyleetcode/findContestMatch.cpp
@@ -0,0 +1,22 @@
+/*
+ *  https://leetcode.com/problems/output-contest-matches/submissions/
+ *  (c) Mohammad Hasanzadeh Mofrad, 2019
+ *  (e) m.hasanzadeh.mofrad@gmail.com
+ */
+class Solution {
+public:
+    string findContestMatch(int n) {
+        std::vector<std::string> teams(n);
+        for (int i = 1; i <= n; i++) {
+            teams[i-1] = to_string(i);
+        }
+
+        for (; n > 1; n /= 2) {
+            for (int i = 0; i < n / 2; i++) {
+                teams[i] = '(' + teams[i] + ',' + teams[n-1-i] + ')';
+            }
+        }
+
+        return(teams[0]);
+    }
+};
